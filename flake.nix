acb6d21b1698d68906b2bd2d148a47b620137785
Philip Taron <philip@qumulo.com>
2022-07-13 13:05:07 -0700

    Add in nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@ rec {
   # Description of the flake.
   description = "Flake Test";
 
-  #inputs = {
-  #};
+  inputs.nixpkgs = {
+    url = "github:nixos/nixpkgs/22.05";
+  };
 
-  outputs = { self }: {
+  outputs = inp: {
     # Nifty: you can use `nix eval .#self` to see what self is.
-    inherit self;
+    inherit (inp) self;
   };
 }
