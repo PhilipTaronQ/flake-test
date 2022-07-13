66ce0556124db8d41de85a07e58f8c8c710ef365
Philip Taron <philip@qumulo.com>
2022-07-13 14:17:17 -0700

    Switch channels

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@ rec {
   description = "Flake Test";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
