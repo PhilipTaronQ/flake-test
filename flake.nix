2c7db9ac67b973fd7b03c07e453084ed77f17dcf
Philip Taron <philip@qumulo.com>
2022-07-13 12:36:00 -0700

    Add a description

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
-  inputs = {
-  };
+  # Description of the flake.
+  description = "Flake Test";
+
+  #inputs = {
+  #};
 
   outputs = { self }: {
-
+    # Nifty: you can use `nix eval .#self` to see what self is.
+    inherit self;
   };
 }
